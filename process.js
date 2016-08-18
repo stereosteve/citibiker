e5611ac157f56c5c103085ade9ffc7cf5baa7972
Steve Perkins <stereosteve@gmail.com>
2016-08-17 21:20:27 -0400

    Parse duration string into hours float

--- process.js
+++ process.js
@@ -19,9 +19,29 @@ files.forEach(function (file) {
     trip.duration = $(this).find('.ed-table__item__info_trip-duration').text().trim()
     trip.cost = $(this).find('.ed-table__col_trip-cost').text().trim()
 
+    trip.durationHours = convertDuration(trip.duration)
+
+    // One time I didn't dock a bike properly and eneded up with a 13 hour ride.
+    // Assume that rides longer than 6 hours are bogus
+    if (trip.durationHours > 6) return
+
     trips.push(trip)
   })
 
 })
+
+function convertDuration(dur) {
+      var s = parseInt(dur.match(/(\d+) s/))
+      var m = parseInt(dur.match(/(\d+) min/))
+      var h = parseInt(dur.match(/(\d+) h/))
+
+      var seconds = 0
+      if (s) seconds += s
+      if (m) seconds += m * 60
+      if (h) seconds += h * 3600
+
+      return seconds / 3600
+}
+
 
 fs.writeFileSync("trips.json", JSON.stringify(trips, undefined, 2))
